929cde28a3a882a600c1c8a3c3723fb8bb6ca4d9
DaniPineda7687 <danielspineda87@gmail.com>
2022-11-24 20:12:56 -0500

    ubicaciones colegios

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,13 +1,36 @@
 import ReactMapGL, { GeolocateControl, Marker, NavigationControl } from "react-map-gl"
 import "mapbox-gl/dist/mapbox-gl.css"
-
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css"
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import Geocoder from "./Geocoder";
 import { LocationContext } from "../LocationProvider";
+import { colegios } from "../colegios";
 export default function Map(){
     const [location, dispatch] = useContext(LocationContext);
-    console.log(location)
+    console.log(colegios)
+
+
+    const pins = useMemo(
+        () =>
+          colegios.map((item, index) => (
+            
+            <Marker
+              key={`marker-${index}`}
+              longitude={item.geometry[1]}
+              latitude={item.geometry[0]}
+              anchor="bottom"
+            >
+            </Marker>
+          )),
+        []
+      );
+/**
+ * <Marker
+            latitude={location.lat}
+            longitude={location.lng}
+            draggable 
+            />
+ */
     return(
         <ReactMapGL
           mapboxAccessToken='pk.eyJ1IjoiZGFuaXBpbmVkYTc2ODciLCJhIjoiY2xhdm1rd3IwMDczdTNzbXoyZXJhN29taCJ9.30hpkvZsDhbXyboZaVMtCw'
@@ -18,11 +41,9 @@ export default function Map(){
           }}
           mapStyle="mapbox://styles/mapbox/streets-v11"
         >
-            <Marker
-            latitude={location.lat}
-            longitude={location.lng}
-            draggable 
-            />
+          
+            {pins}
+            
             <NavigationControl position='bottom-right'/>
             <GeolocateControl
               position='top-left'
