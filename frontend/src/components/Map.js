a8640e1e1dd0ff63e51cd90adaa911a647219baa
DaniPineda7687 <danielspineda87@gmail.com>
2022-11-24 22:21:11 -0500

    gg

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -83,7 +83,21 @@ export default function Map(){
                     <div className="more__information__container">
                         <h2>{popupInfo.nombre}</h2> 
                         <table>
-                            
+                            <tr>
+                                <th>Company</th>
+                                <th>Contact</th>
+                                <th>Country</th>
+                            </tr>
+                            <tr>
+                                <td>Alfreds Futterkiste</td>
+                                <td>Maria Anders</td>
+                                <td>Germany</td>
+                            </tr>
+                            <tr>
+                                <td>Centro comercial Moctezuma</td>
+                                <td>Francisco Chang</td>
+                                <td>Mexico</td>
+                            </tr>
                         </table>
                     </div>
                 : 
