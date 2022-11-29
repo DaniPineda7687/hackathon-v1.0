235eac2b2c58a3efc757373098c3bc35e5435304
RubenU2002 <rsantiagourrego@gmail.com>
2022-11-29 15:54:44 -0500

    getOptimalRoute service y llamado de prueba

---
+++ frontend/src/services/getOptimalRoute.js
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+const token = 'pk.eyJ1IjoiZGFuaXBpbmVkYTc2ODciLCJhIjoiY2xhdm1rd3IwMDczdTNzbXoyZXJhN29taCJ9.30hpkvZsDhbXyboZaVMtCw'
+export const getOptimalRoute= async({userPosition=[1,1],schoolPosition=[1,1],method="driving"}={})=>{
+    const [userLatitude,userLongitude] = userPosition;
+    const [schoolLatitude,schoolLongitude] = schoolPosition;
+    const response = await axios.get(`https://api.mapbox.com/directions/v5/mapbox/${method}/${userLongitude},${userLatitude};${schoolLongitude},${schoolLatitude}?geometries=geojson&access_token=${token}`);
+    const route = await response.data;
+    const routeGeometry= route.routes[0].geometry;
+    
+    return routeGeometry;
+}
+
