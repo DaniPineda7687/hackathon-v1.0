ef2d1fa49bda279e92535ca800a2d7ff2936b273
DaniPineda7687 <danielspineda87@gmail.com>
2022-11-30 23:32:03 -0500

    rutas y popup desaparecen unicamente cuando el colegio esta fuera del rango

--- frontend/src/services/utils.js
+++ frontend/src/services/utils.js
@@ -1,5 +1,6 @@
 function textFormatter(str){
-    let words = str.split(" ");
+    let strLowerCase = str.toLowerCase();
+    let words = strLowerCase.split(" ");
     let newText = words.map(word=>{
       let newL = word[0].toUpperCase()
       return word.replace(word[0],newL)
@@ -7,4 +8,11 @@ function textFormatter(str){
     }).join(" ")
     return newText;
 }
-export default textFormatter;
+
+function findSchool(schools,position){
+  return schools.find(school=>{
+    return school.geometry[0]==position[0]&&school.geometry[1]==position[1]
+  })
+}
+
+export {textFormatter,findSchool}
